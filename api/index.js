db81272dbbb91965f81351e94ea30712df59e76d
anilgyawali0623 <anilgyawali8937@gmail.com>
2024-06-22 16:47:26 +0545

    new change

--- api/index.js
+++ api/index.js
@@ -2,7 +2,7 @@ import express from "express";
 import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRoutes from "./routes/user.route.js";
- import authRoutes from './routes/auth.route.js';
+import authRoutes from "./routes/auth.route.js";
 dotenv.config();
 const app = express();
 mongoose
@@ -13,7 +13,7 @@ mongoose
   .catch((err) => {
     console.log(err);
   });
-   app.use(express.json());
+app.use(express.json());
 app.listen(3000, () => {
   console.log("server is running on port 3000");
 });
@@ -21,3 +21,12 @@ app.listen(3000, () => {
 app.use("/api/user", userRoutes);
 app.use("/api/auth", authRoutes);
 
+app.use((err, req, res, next) => {
+  const statusCode = err.statusCode || 500;
+  const message = err.message || "internal sever error";
+  res.status(statusCode).json({
+    success: false,
+    statusCode,
+    message,
+  });
+});
