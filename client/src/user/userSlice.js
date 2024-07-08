a1f1750887c028e85271e840d74fa2433e5312d5
anilgyawali0623 <anilgyawali8937@gmail.com>
2024-07-08 11:34:29 +0545

    complete delete user account functionlaity

--- client/src/user/userSlice.js
+++ client/src/user/userSlice.js
@@ -24,20 +24,41 @@ const userSlice = createSlice({
       state.error = action.payload;
     },
     updateStart: (state, action) => {
- state.loading= true,
-  state.error= null
+      (state.loading = true), (state.error = null);
     },
-      updateSuccess: (state,action)=>{
-         state.currentUser=action.payload,
-         state.loading= false,
-          state.error=null
-      },
-       updateFailure:(state, action)=>{
-         state.loading= false,
-          state.error= action.payload 
-       }
+    updateSuccess: (state, action) => {
+      (state.currentUser = action.payload),
+        (state.loading = false),
+        (state.error = null);
+    },
+    updateFailure: (state, action) => {
+      (state.loading = false), (state.error = action.payload);
+    },
+    deleteUserStart: (state) => {
+      state.loading = true;
+      state.error = null;
+    },
+    deleteUserSuccess: (state) => {
+      state.currentUser = null;
+      state.loading = false;
+      state.error = null;
+    },
+    deleteUserFailure: (state, action) => {
+      state.loading = false;
+      state.error = action.payload;
+    },
   },
 });
 
-export const { signInFailure, signInStart, signInSuccess, updateFailure, updateStart, updateSuccess } = userSlice.actions;
+export const {
+  signInFailure,
+  signInStart,
+  signInSuccess,
+  updateFailure,
+  updateStart,
+  updateSuccess,
+  deleteUserStart,
+  deleteUserSuccess,
+  deleteUserFailure
+} = userSlice.actions;
 export default userSlice.reducer;
